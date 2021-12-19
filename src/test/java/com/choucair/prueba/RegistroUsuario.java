465999749cdc223c992ea32348a5fc8930f6162f
dacevedo1 <dacevedo@fortesza.com>
2021-12-19 13:40:50 -0500

    Primer commit

---
+++ src/test/java/com/choucair/prueba/RegistroUsuario.java
@@ -0,0 +1,36 @@
+package com.choucair.prueba;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegistroUsuario {
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com/index.php");
+	}
+	
+	@Test
+	public void prueba() throws InterruptedException {
+		WebElement signInButton = driver.findElement(By.className("login"));
+		signInButton.click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@After
+	public void tearDown() {
+		//driver.quit();
+	}
+}
